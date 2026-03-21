215f5f7e4ac0cf725cc3060612347b9922a487d7
Arthur <33087612+arthurbaron@users.noreply.github.com>
2026-03-21 18:20:38 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # name: discourse-football-formation
 # about: Renders football/soccer formation diagrams from BBCode tags
-# version: 1.0.0
-# authors: Arsenal Supporters Forum
+# version: 1.1.0
+# authors: Online Arsenal Community
 # url: https://github.com/arthurbaron/discourse-football-formation
 
 register_asset "stylesheets/formation.css"
